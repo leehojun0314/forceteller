83ca7f962994a4b82f74d41ecf538d2f8da40018
leehojun0314 <dlghwns0314@naver.com>
2024-06-15 22:51:35 +1000

    nav bar working

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 export default function Layout() {
   return (
-    <div className="mx-auto h-screen max-w-3xl">
+    <div className="mx-auto max-w-3xl">
       <Header />
       <Outlet />
     </div>
